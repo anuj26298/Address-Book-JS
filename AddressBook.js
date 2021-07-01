dade4506f5bc67d72ad629e36a62a0efe2ce8661
anuj26298 <kumar26298anuj@gmail.com>
2021-07-01 16:16:43 +0530

    ADDED code for adding address book and contacts to address book

--- AddressBook.js
+++ AddressBook.js
@@ -1,5 +1,13 @@
-class Person{
-    constructor(firstName,lastName,address,city,state,zipCode,phone,email){
+class Person {
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zipCode;
+    phone;
+    email;
+    constructor(firstName, lastName, address, city, state, zipCode, phone, email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -10,59 +18,104 @@ class Person{
         this.email = email;
     }
 
-    set firstName(firstName){
-        this.firstName = firstName;
+    set firstName(firstName) {
+        let regex = RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if (regex.test(firstName)) {
+            this.firstName = firstName;
+        }
     }
-    get firstName(){
+    get firstName() {
         return this.firstName;
     }
-    
-    set lastName(lastName){
-        this.lastName = lastName;
+
+    set lastName(lastName) {
+        let regex = RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if (regex.test(this.firstName)) {
+            this.lastName = lastName;
+        }
     }
-    get lastName(){
+    get lastName() {
         return this.lastName;
     }
-    set address(address){
-        this.address = address;
+    set address(address) {
+        let regex = RegExp("^[A-Za-z]{5,}$");
+        if (regex.test(address)) {
+            this.address = address;
+        }
     }
 
-    get address(){
+    get address() {
         return this.address;
     }
 
-    set city(city){
-        this.city = city;
+    set city(city) {
+        let regex = RegExp("^[A-Za-z]{3,}$");
+        if (regex.test(city)) {
+            this.city = city;
+        }
     }
-    get city(){
+    get city() {
         return this.city;
     }
 
-    set state(state){
-        this.state = state;
+    set state(state) {
+        let regex = RegExp("^[A-Za-z]{3,}$");
+        if (regex.test(state)) {
+            this.state = state;
+        }
     }
-    get state(){
+    get state() {
         return this.state;
     }
 
-    set zipCode(zipCode){
-        this.zipCode = zipCode;
+    set zipCode(zipCode) {
+        let regex = RegExp("[0-9]{6}");
+        if (regex.test(zipCode)) {
+            this.zipCode = zipCode;
+        }
     }
-    get zipCode(){
+    get zipCode() {
         return this.zipCode;
     }
 
-    set phone(phone){
-        this.phone = phone;
+    set phone(phone) {
+        let regex = RegExp("^\s(?:\+?(\d{1,3}))?[-.(]*(\d{3})[-.)]*(\d{3})[-.]*(\d{4})(?:*x(\d+))?\s*");
+        if (regex.test(phone)) {
+            this.phone = phone;
+        }
     }
-    get phone(){
+    get phone() {
         return this.phone;
     }
 
-    set email(email){
-        this.email = email;
+    set email(email) {
+        let regex = RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        if (regex.test(email)) {
+            this.email = email;
+        }
     }
-    get email(){
+    get email() {
         return this.email;
     }
 }
+
+class AddressBook {
+    addressBookArray = [];
+
+    addContact(person) {
+        this.addressBookArray.push(person);
+    }
+
+    showContacts() {
+        addressBook.addressBookArray.forEach(person => {
+            console.log(" \nFirstName : " + person.firstName + "\nLastName : " + person.lastName + "\n Address: " + person.address +
+                "\nCity : " + person.city + "\nState : " + person.state + "\nZipCode : " + person.zipCode + "\nPhone : " + 
+                person.phone + "\nemail : " + person.email);
+        });
+    }
+}
+
+let addressBook = new AddressBook();
+var person = new Person("Abhi", "Kapoor", "VedViyas", "Meerut", "UP", "250005","9876543210","abhi.kapoor@example.com");
+addressBook.addContact(person);
+addressBook.showContacts();
